245c1b28a05048c939100ebb284d69beb226baaf
Hessam <hessam.modaberi@gmail.com>
2018-06-09 18:06:22 +0430

    change long to double for task runtime2

--- optimization-framework/src/main/java/org/optframework/core/TaskUtility.java
+++ optimization-framework/src/main/java/org/optframework/core/TaskUtility.java
@@ -4,7 +4,7 @@ import org.cloudbus.cloudsim.util.workload.Job;
 import org.cloudbus.spotsim.enums.InstanceType;
 
 public class TaskUtility {
-    public static long executionTimeOnType(Job job, InstanceType type){
+    public static double executionTimeOnType(Job job, InstanceType type){
         return job.getLength() / type.getEc2units();
     }
 }
