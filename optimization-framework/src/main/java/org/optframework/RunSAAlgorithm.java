3d9fdefd07f1347e961b839989ce6a0422203d35
Hessam <hessam.modaberi@gmail.com>
2018-06-13 14:38:05 +0430

    adds printer class

--- optimization-framework/src/main/java/org/optframework/RunSAAlgorithm.java
+++ optimization-framework/src/main/java/org/optframework/RunSAAlgorithm.java
@@ -54,7 +54,7 @@ public class RunSAAlgorithm implements StaticProperties {
 
         long stop = System.currentTimeMillis();
 
-        printSolution(solution, instanceInfo,stop-start);
+        Printer.printSolution(solution, instanceInfo,stop-start);
     }
 
     private static InstanceInfo[] populateInstancePrices(Region region , AZ az, OS os){
@@ -71,55 +71,5 @@ public class RunSAAlgorithm implements StaticProperties {
             info[type.getId()] = instanceInfo;
         }
         return info;
-    }
-
-    private static void printSolution(Solution solution, InstanceInfo instanceInfo[], long time){
-        Log.logger.info("Number of used Instances: " + solution.numberOfUsedInstances);
-
-        for (int i = 0; i < solution.instanceTimes.length; i++) {
-            Log.logger.info("Requested time for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimes[i]);
-        }
-
-        for (int i = 0; i < solution.instanceTimes.length; i++) {
-            Log.logger.info("Timeline for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimelines[i]);
-        }
-
-        String xArray = "";
-        for (int val : solution.xArray){
-            xArray += " " + String.valueOf(val);
-        }
-        Log.logger.info("Value of the X Array: "+ xArray);
-
-        String yArray = "";
-        for (int i = 0; i < solution.numberOfUsedInstances; i++) {
-            yArray += " " + String.valueOf(solution.yArray[i]);
-        }
-        Log.logger.info("Value of the Y Array: "+ yArray);
-
-        Log.logger.info("Total Cost: " + solution.cost);
-        Log.logger.info("Makespan: " + solution.makespan);
-        Log.logger.info("Fitness Value: "+ solution.fitnessValue);
-        String timePrefix;
-        long sec = time/1000;
-        long min = sec/60;
-        long hr = min/60;
-
-        long converted;
-
-        if (sec < 0){
-            timePrefix = "Milisec";
-            converted = time;
-        }else if (min < 1){
-            timePrefix = "Seconds";
-            converted = sec;
-        }else if (hr < 1){
-            timePrefix = "Minutes";
-            converted = min;
-        }else {
-            timePrefix = "Hours";
-            converted = hr;
-        }
-
-        Log.logger.info("Algorithm runtime: "+ converted + " "+ timePrefix + " ["+time+"]");
     }
 }
---
+++ optimization-framework/src/main/java/org/optframework/core/Printer.java
@@ -0,0 +1,53 @@
+package org.optframework.core;
+
+public class Printer {
+    public static void printSolution(Solution solution, InstanceInfo instanceInfo[], long time){
+        Log.logger.info("Number of used Instances: " + solution.numberOfUsedInstances);
+
+        for (int i = 0; i < solution.instanceTimes.length; i++) {
+            Log.logger.info("Requested time for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimes[i]);
+        }
+
+        for (int i = 0; i < solution.instanceTimes.length; i++) {
+            Log.logger.info("Timeline for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimelines[i]);
+        }
+
+        String xArray = "";
+        for (int val : solution.xArray){
+            xArray += " " + String.valueOf(val);
+        }
+        Log.logger.info("Value of the X Array: "+ xArray);
+
+        String yArray = "";
+        for (int i = 0; i < solution.numberOfUsedInstances; i++) {
+            yArray += " " + String.valueOf(solution.yArray[i]);
+        }
+        Log.logger.info("Value of the Y Array: "+ yArray);
+
+        Log.logger.info("Total Cost: " + solution.cost);
+        Log.logger.info("Makespan: " + solution.makespan);
+        Log.logger.info("Fitness Value: "+ solution.fitnessValue);
+        String timePrefix;
+        long sec = time/1000;
+        long min = sec/60;
+        long hr = min/60;
+
+        long converted;
+
+        if (sec < 0){
+            timePrefix = "Milisec";
+            converted = time;
+        }else if (min < 1){
+            timePrefix = "Seconds";
+            converted = sec;
+        }else if (hr < 1){
+            timePrefix = "Minutes";
+            converted = min;
+        }else {
+            timePrefix = "Hours";
+            converted = hr;
+        }
+
+        Log.logger.info("Algorithm runtime: "+ converted + " "+ timePrefix + " ["+time+"]");
+    }
+}
