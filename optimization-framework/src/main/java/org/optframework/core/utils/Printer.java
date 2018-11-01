7d67582a728c640dc65242bd0f6eba0b88c39e0d
Hessam Modaberi <hessam.modaberi@gmail.com>
2018-11-01 03:50:05 +0330

    refactor: refactors the printer class

--- optimization-framework/src/main/java/org/optframework/core/utils/Printer.java
+++ optimization-framework/src/main/java/org/optframework/core/utils/Printer.java
@@ -122,15 +122,20 @@ public class Printer {
     }
 
     public static void printSolutionWithouthTime(Solution solution, InstanceInfo instanceInfo[]){
-        Log.logger.info("Number of used Instances: " + solution.numberOfUsedInstances);
+        String toPrint = "";
+        toPrint += "Number of used Instances: " + solution.numberOfUsedInstances + "\n\n";
+        toPrint += "======================================================================";
 
         for (int i = 0; i < solution.instanceTimes.length; i++) {
-            Log.logger.info("Requested time for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimes[i]);
+            toPrint += "Requested time for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimes[i] + "\n";
         }
+        toPrint += "======================================================================";
 
         for (int i = 0; i < solution.instanceTimelines.length; i++) {
-            Log.logger.info("Timeline for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimelines[i]);
+            toPrint += "Timeline for instance " + instanceInfo[solution.yArray[i]].getType().getName() + " : " + solution.instanceTimelines[i] + "\n";
         }
+
+        Log.logger.info(toPrint);
 
         String xArray = "";
         for (int val : solution.xArray){
@@ -144,9 +149,11 @@ public class Printer {
         }
         Log.logger.info("Value of the Y Array: "+ yArray);
 
-        Log.logger.info("Total Cost: " + solution.cost);
-        Log.logger.info("Makespan: " + solution.makespan);
-        Log.logger.info("Fitness Value: "+ solution.fitnessValue);
+        toPrint = "Total Cost: " + solution.cost + "\n";
+        toPrint += "Makespan: " + solution.makespan + "\n";
+        toPrint += "Fitness Value: "+ solution.fitnessValue + "\n";
+
+        Log.logger.info(toPrint);
     }
 
     public static void printUtilization(double utilization[]){
@@ -159,10 +166,10 @@ public class Printer {
         String str = "";
 
         for (int i = 0; i < utilization.length; i++) {
-            str += "Instance" + i + ": "+ utilization[i] + "\n\n";
+            str += "Instance" + i + ": "+ utilization[i] + "\n";
         }
 
-        str += "AVERAGE RESOURCE UTILIZATION: " + utilizationAverage;
+        str += "\nAVERAGE RESOURCE UTILIZATION: " + utilizationAverage;
 
         Log.logger.info(str);
     }
