01cba54390e1dd16ac3ae8e9e48d7f7701d6b3a2
Hessam Modaberi <hessam.modaberi@gmail.com>
2018-11-09 03:57:19 +0330

    feat: loss2 algorithm

--- optimization-framework/src/main/java/org/optframework/core/lossandgain/Loss2Algorithm.java
+++ optimization-framework/src/main/java/org/optframework/core/lossandgain/Loss2Algorithm.java
@@ -1,22 +1,73 @@
 package org.optframework.core.lossandgain;
 
-import org.optframework.core.OptimizationAlgorithm;
-import org.optframework.core.Solution;
+import org.optframework.core.*;
 
 public class Loss2Algorithm implements OptimizationAlgorithm {
 
     Solution heftSolution;
+    int totalInstances[];
+    Workflow workflow;
+    InstanceInfo instanceInfo[];
 
-    public Loss2Algorithm(Solution heftSolution) {
-        this.heftSolution = heftSolution;
+    public Loss2Algorithm(Solution heftSolution, int[] totalInstances, Workflow workflow, InstanceInfo[] instanceInfo) {
+        try {
+            this.heftSolution = heftSolution.clone();
+        }catch (Exception e){
+            Log.logger.info("Clone Exception");
+        }
+        this.totalInstances = totalInstances;
+        this.workflow = workflow;
+        this.instanceInfo = instanceInfo;
     }
 
     @Override
     public Solution runAlgorithm() {
+        double matrix[][] = new double[totalInstances.length][workflow.getJobList().size()];
+
+        /**
+         * This part is for calculating matrix elements
+         * */
+        for (int i = 0; i < workflow.getJobList().size(); i++) {
+            for (int j = 0; j < totalInstances.length; j++) {
+                double oldMakespan = heftSolution.makespan;
+                double oldCost = heftSolution.cost;
+                int tempX = heftSolution.xArray[i];
+
+                heftSolution.xArray[i] = j;
+
+                heftSolution.fitness();
 
 
+                double newMakespan = heftSolution.makespan;
+                double newCost = heftSolution.cost;
 
+                matrix[j][i] = (newMakespan - oldMakespan) / (oldCost - newCost);
 
-        return null;
+                heftSolution.xArray[i] = tempX;
+            }
+        }
+
+        /**
+         * Generating loss solution from matrix
+         * */
+        int newXArray[] = new int[heftSolution.xArray.length];
+
+        for (int i = 0; i < workflow.getJobList().size(); i++) {
+            double minValue = 99999999999.999;
+            int minInstanceId = -1;
+            for (int j = 0; j < totalInstances.length; j++) {
+                if (matrix[j][i] < minValue){
+                    minValue = matrix[j][i];
+                    minInstanceId = j;
+                }
+            }
+            newXArray[i] = minInstanceId;
+        }
+
+        heftSolution.xArray = newXArray;
+
+        heftSolution.fitness();
+
+        return heftSolution;
     }
 }
