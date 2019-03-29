9436448a5c14020bae993d67c83d84de6cb3d944
Hessam <hessam.modaberi@gmail.com>
2019-03-29 22:07:47 +0430

    cleans GRPHEFTAlgorithm class from unncessary logics

--- optimization-framework/src/main/java/org/optframework/core/grpheft/GRPHEFTAlgorithm.java
+++ optimization-framework/src/main/java/org/optframework/core/grpheft/GRPHEFTAlgorithm.java
@@ -35,39 +35,8 @@ public class GRPHEFTAlgorithm implements OptimizationAlgorithm{
         GlobalAccess.orderedJobList = cloner.deepClone(workflow.getJobList());
         Collections.sort(GlobalAccess.orderedJobList, Job.rankComparator);
 
-        //todo:...
+        //Initializes M_Number with number of tasks
         Config.global.m_number = workflow.getJobList().size();
-
-        Log.logger.info("<<<<<<<<<<  HEFT Algorithm is started  >>>>>>>>>>>");
-        int totalInstances[] = HEFTAlgorithm.getTotalInstancesForHEFT(3, instanceInfo);
-
-        Workflow heftWorkflow = PreProcessor.doPreProcessingForHEFT(PopulateWorkflow.populateWorkflowWithId(Config.global.budget, 0, Config.global.workflow_id), Config.global.bandwidth, totalInstances, instanceInfo);
-
-        heftWorkflow.setBeta(Beta.computeBetaValue(heftWorkflow, instanceInfo, Config.global.m_number));
-
-        HEFTAlgorithm heftAlgorithm = new HEFTAlgorithm(heftWorkflow, instanceInfo, totalInstances);
-        Solution heftSolution = heftAlgorithm.runAlgorithm();
-        heftSolution.heftFitness();
-
-        heftSolution.fitness();
-        Printer.lightPrintSolution(heftSolution , 0);
-
-        Loss2Algorithm loss2Algorithm = new Loss2Algorithm(heftSolution, totalInstances, workflow, instanceInfo);
-        Solution loss2Solution = loss2Algorithm.runAlgorithm();
-
-
-        Loss3Algorithm loss3Algorithm = new Loss3Algorithm(heftSolution, totalInstances, workflow, instanceInfo);
-        Solution loss3Solution = heftSolution;
-        if(!Config.global.deadline_based) {
-            loss3Solution = loss3Algorithm.runAlgorithm2();
-        }
-
-        /**
-         * Compute the maximum number of used instances
-         * */
-        loss3Solution.solutionMapping();
-        loss2Solution.solutionMapping();
-        heftSolution.solutionMapping();
 
         List<Job> orderedJobList = GlobalAccess.orderedJobList;
 
@@ -85,27 +54,12 @@ public class GRPHEFTAlgorithm implements OptimizationAlgorithm{
             Config.global.m_number = originalMNumber;
         }
 
-        computeCoolingFactorForSA(workflow.getJobList().size());
-
-
         workflow.setBeta(Beta.computeBetaValue(workflow, instanceInfo, Config.global.m_number));
 
         Integer zArray[] = new Integer[orderedJobList.size()];
         for (int i = 0; i < orderedJobList.size(); i++) {
             zArray[i] = orderedJobList.get(i).getIntId();
         }
-
-        loss2Solution.zArray = zArray;
-        heftSolution.zArray = zArray;
-
-        loss2Solution.maxNumberOfInstances = Config.global.m_number;
-        loss3Solution.maxNumberOfInstances = Config.global.m_number;
-        heftSolution.maxNumberOfInstances = Config.global.m_number;
-
-        heftSolution.origin = "heft";
-        loss2Solution.origin = "loss2";
-        loss3Solution.origin = "loss3";
-
 
         /**
          * HEFT algorithm which is limited by m_number
@@ -141,82 +95,6 @@ public class GRPHEFTAlgorithm implements OptimizationAlgorithm{
     public static double get_price_per_unit(int InstanceId, InstanceInfo instanceInfo[])
     {
         return instanceInfo[InstanceId].getType().getEcu()/instanceInfo[InstanceId].getSpotPrice() ;
-    }
-
-    public static int get_next_baised_instance_id(InstanceInfo instanceInfo[])
-    {
-        double sum = 0;
-        for (int id=0; id<instanceInfo.length;id++) {
-            sum += get_price_per_unit(id,instanceInfo)+instanceInfo[id].getType().getEcu();
-        }
-
-        Random rand = new Random();
-        double randomY = rand.nextDouble();
-        double probabilitySumTemp = 0;
-        int selectedInstance = -1;
-
-        for (int i = instanceInfo.length - 1; i >=0 ; i--) {
-            probabilitySumTemp += (get_price_per_unit(i, instanceInfo)+instanceInfo[i].getType().getEcu())/ sum;
-            if (probabilitySumTemp > randomY) {
-                selectedInstance = i;
-                break;
-            }
-        }
-
-        return selectedInstance;
-    }
-
-    public static int Calculating_M_number_For_PACSA_Plus_With_Biase(double minInstancePrice,InstanceInfo instanceInfo[])
-    {
-
-        int totalInstances2[] = new int[0];
-        double remainingBudget = Config.global.budget;
-
-
-        while (minInstancePrice <= remainingBudget && totalInstances2.length < Config.global.m_number){
-            double maxValidCost = 0.0;
-            int instanceTypeId = -2;
-
-            Random r = new Random();
-            int randomType;
-
-            randomType = get_next_baised_instance_id(instanceInfo);//r.nextInt(InstanceType.values().length);
-            while (instanceInfo[randomType].getSpotPrice() > remainingBudget)
-            {
-                randomType = get_next_baised_instance_id(instanceInfo); //r.nextInt(InstanceType.values().length);
-            }
-            maxValidCost = instanceInfo[randomType].getSpotPrice();
-            instanceTypeId = randomType;
-
-
-            int newTotalInstance[] = new int[totalInstances2.length + 1];
-            for (int i = 0; i < totalInstances2.length; i++) {
-                newTotalInstance[i] = totalInstances2[i];
-            }
-
-            newTotalInstance[totalInstances2.length] = instanceTypeId;
-            totalInstances2 = newTotalInstance;
-
-            remainingBudget -= maxValidCost;
-
-
-            DecimalFormat df = new DecimalFormat ("#.#####");
-            remainingBudget = Double.parseDouble(df.format(remainingBudget));
-
-
-        }
-
-        return totalInstances2.length;
-    }
-
-    static void computeCoolingFactorForSA(int numberOfTasks){
-        if (!Config.sa_algorithm.force_cooling){
-            if (numberOfTasks >= 10){
-                Config.sa_algorithm.cooling_factor = 1 - 1 / (double)numberOfTasks;
-            }else {
-                Config.sa_algorithm.cooling_factor = 0.9;
-            }
-        }
     }
 
     /**
