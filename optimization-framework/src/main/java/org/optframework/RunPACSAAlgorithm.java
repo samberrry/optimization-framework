4777d251ac154b8e75514e623db295b035921328
Hessam <hessam.modaberi@gmail.com>
2019-04-25 14:24:49 +0430

    adds GRP-HEFT algorithm

--- optimization-framework/src/main/java/org/optframework/RunPACSAAlgorithm.java
+++ optimization-framework/src/main/java/org/optframework/RunPACSAAlgorithm.java
@@ -7,6 +7,7 @@ import org.cloudbus.spotsim.enums.OS;
 import org.cloudbus.spotsim.enums.Region;
 import org.optframework.config.Config;
 import org.optframework.core.*;
+import org.optframework.core.grpheft.GRPHEFTAlgorithm;
 import org.optframework.core.heft.HEFTAlgorithm;
 import org.optframework.core.lossandgain.Loss2Algorithm;
 import org.optframework.core.lossandgain.Loss3Algorithm;
@@ -136,31 +137,12 @@ public class RunPACSAAlgorithm {
             loss2Solution.origin = "loss2";
             loss3Solution.origin = "loss3";
 
-            /**
-             * HEFT algorithm which is limited by m_number
-             * */
+            RunGRPHEFTAlgorithm.thisTypeIsUsedAsMaxEfficient = new boolean[instanceInfo.length];
+            GRPHEFTAlgorithm grpheftAlgorithm = new GRPHEFTAlgorithm(instanceInfo);
+            Solution grpHeftSolution = grpheftAlgorithm.runAlgorithm();
 
-            int id_fastest_instance = 7;
-            double cost_fastest_instance = instanceInfo[id_fastest_instance].getSpotPrice();
-
-            int number_of_affordable_fastest_instance = (int)((Config.global.budget)/cost_fastest_instance);
-
-            Log.logger.info("Number of affordable fastest instances is:"+number_of_affordable_fastest_instance);
-
-            int totalInstances3[] = greedyResourceProvisioning(instanceInfo, number_of_affordable_fastest_instance, minPrice, cost_fastest_instance);
-
-            Workflow heftWorkflow3 = PreProcessor.doPreProcessingForHEFT(PopulateWorkflow.populateWorkflowWithId(Config.global.budget, 0, Config.global.workflow_id), Config.global.bandwidth, totalInstances3, instanceInfo);
-
-            heftWorkflow3.setBeta(Beta.computeBetaValue(heftWorkflow3, instanceInfo, Config.global.m_number));
-
-            HEFTAlgorithm heftAlgorithm3 = new HEFTAlgorithm(heftWorkflow3, instanceInfo, totalInstances3, Config.global.m_number);
-            Solution heftSolution3 = heftAlgorithm3.modified_heft_runAlgorithm();
-            //todo: here you can test the modified heft algorithm
-            modifiedHeftMNumber = heftSolution3.maxNumberOfInstances;
-
-            initialSolutionList.add(heftSolution3);
-            Printer.printSolutionWithouthTime(heftSolution3, instanceInfo);
-
+            modifiedHeftMNumber = grpHeftSolution.numberOfUsedInstances;
+            initialSolutionList.add(grpHeftSolution);
         }
 
         long runTimeSum = 0;
@@ -415,53 +397,5 @@ public class RunPACSAAlgorithm {
                 Config.sa_algorithm.cooling_factor = 0.9;
             }
         }
-    }
-
-    /**
-     * Greedy Resource Provisioning Algorithm (GRP)
-     * */
-    static int[] greedyResourceProvisioning(InstanceInfo instanceInfo[], int number_of_affordable_fastest_instance, double minPrice, double cost_fastest_instance){
-        int totalInstances[];
-        switch (Config.global.workflow_id){
-            case 30:
-            case 31:
-            case 32:
-            case 33:
-            case 34:
-            case 35:
-                totalInstances = HEFTAlgorithm.getTotalInstancesForHEFTMostPowerful(number_of_affordable_fastest_instance, instanceInfo);
-                break;
-            default:
-                totalInstances = HEFTAlgorithm.getTotalInstancesForHEFTMostPowerful(Min(number_of_affordable_fastest_instance,Config.global.m_number), instanceInfo);
-                break;
-        }
-        Config.global.m_number = totalInstances.length;
-
-        double remainingBudget = Config.global.budget - ((number_of_affordable_fastest_instance) * cost_fastest_instance);
-
-        while (minPrice <= remainingBudget && totalInstances.length < Config.global.m_number) {
-            //    Log.logger.info("TotalInstances Length is:"+totalInstances3.length);
-            double maxValidCost = 0.0;
-            int instanceTypeId = -2;
-            for (int instance_id = instanceInfo.length-1; instance_id >=0; instance_id--) {//for (int instance_id: sorted_instanceTypes_based_on_Cost_per_ComputeUnit) {
-                if (instanceInfo[instance_id].getSpotPrice() <= remainingBudget && instanceInfo[instance_id].getSpotPrice() >= maxValidCost) {
-                    maxValidCost = instanceInfo[instance_id].getSpotPrice();
-                    instanceTypeId = instance_id;
-                    break;
-                }
-            }
-
-            int newTotalInstance[] = new int[totalInstances.length + 1];
-            for (int i = 0; i < totalInstances.length; i++) {
-                newTotalInstance[i] = totalInstances[i];
-            }
-
-            newTotalInstance[totalInstances.length] = instanceTypeId;
-            totalInstances = newTotalInstance;
-
-            remainingBudget -= maxValidCost;
-        }
-
-        return totalInstances;
     }
 }
