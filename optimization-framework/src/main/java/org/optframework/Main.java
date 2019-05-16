75e430ac21f9a700d841a1e8cbabf4dbe783fe33
Hessam <hessam.modaberi@gmail.com>
2019-05-17 00:24:00 +0430

    partial - setups grp pacsa run algorithm

--- optimization-framework/src/main/java/org/optframework/Main.java
+++ optimization-framework/src/main/java/org/optframework/Main.java
@@ -54,6 +54,9 @@ public class Main {
             case "grp-heft":
                 RunGRPHEFTAlgorithm.runGRPHEFT();
                 break;
+            case "grp-pacsa":
+                RunGRPPACSAAlgorithm.runGRPPACSA();
+                break;
         }
     }
 }
---
+++ optimization-framework/src/main/java/org/optframework/RunGRPPACSAAlgorithm.java
@@ -0,0 +1,12 @@
+package org.optframework;
+
+public class RunGRPPACSAAlgorithm {
+
+    public static void runGRPPACSA(){
+        //todo: should start grp algorithm
+        //print it
+
+        //todo: should start pacsa with initial solution
+        //print it
+    }
+}
